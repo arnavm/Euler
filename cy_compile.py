a574b28c4a6736f1dab2774fe5e3eb6a031ec850
Arnav Moudgil <ocean57@gmail.com>
2011-06-24 00:46:02 -0400

    Script to automatically compile .pyx files to .so files

---
+++ cy_compile.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os, sys, shutil, subprocess
+
+assert len(sys.argv) == 2, "usage: cy_compile.py <file>"
+
+phile = sys.argv[1]
+
+f = open('setup.py', 'w')
+print >> f, "from distutils.core import setup"
+print >> f, "from distutils.extension import Extension"
+print >> f, "from Cython.Distutils import build_ext"
+print >> f, "setup(cmdclass = {'build_ext': build_ext}, ext_modules = [Extension('%s', ['%s'])])" % (phile[:-4], phile)
+f.close()
+
+call = "python setup.py build_ext --inplace"
+subprocess.call(call, shell = True)
+
+shutil.rmtree('build')
+os.remove(phile[:-4] + '.c')
